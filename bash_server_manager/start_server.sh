92436f9b5dfc7e648879c78ec49fcfb5620d0e68
wysixontheflux <bitpoch@icloud.com>
2023-06-20 23:59:21 +0200

    bash test

--- bash_server_manager/start_server.sh
+++ bash_server_manager/start_server.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD=/home/codserver/server2/codextended.so /home/codserver/server2/cod_lnxded +exec config.cfg +set net_ip 193.38.250.89 +set net_port $1 +set fs_homepath /home/codserver/server2 +set fs_basepath /home/codserver/server2
+LD_PRELOAD=../test_server/codextended.so ../test_server/cod_lnxded +set net_port $1 +set fs_homepath ../test_server/ +set fs_basepath../test_server
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000
 
 app.get('/execute', (req, res) => {
     let port = req.query.port;  // Récupère le port depuis les paramètres de la requête
-    exec(`bash start_server.sh ${port}`, (err, stdout, stderr) => {
+    exec(`sudo cd bash_server_manager && sh start_server.sh ${port}`, (err, stdout, stderr) => {
         if (err) {
             console.log(err)
             return
