39e7b91d95ae8d6dc92f2ba59ed670a7d7563d7f
wysixontheflux <bitpoch@icloud.com>
2023-06-21 01:22:21 +0200

    execution server working with http request

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require('express')
-const { exec } = require('child_process')
+const { spawn } = require('child_process')
 
 const app = express()
 const port = 3000
 
 app.get('/execute', (req, res) => {
-    let port = req.query.port;  // Récupère le port depuis les paramètres de la requête
-    exec(`sudo cd bash_server_manager && sh start_server.sh ${port}`, (err, stdout, stderr) => {
-        if (err) {
-            console.log(err)
-            return
-        }
-        console.log(stdout)
+    console.log('appel route')
+    let port = req.query.port;
+    let child = spawn('sh', ['start_server.sh', port], { cwd: 'bash_server_manager' })
+
+    child.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`)
+    })
+
+    child.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`)
+    })
+
+    child.on('error', (error) => {
+        console.error(`error: ${error.message}`)
+    })
+
+    child.on('exit', (code, signal) => {
+        if (code) console.log(`Process exited with code ${code}`)
+        if (signal) console.log(`Process was killed with signal ${signal}`)
     })
 })
 
 app.listen(port, () => {
     console.log(`API démarrée sur http://localhost:${port}`)
 });
+
+
+// http://193.38.250.89:3000/execute?port=28965
